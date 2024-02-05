3b74576aeb4364046abfca412a80fec4e7d861cf
ThilakRamanie <thilakramanie007@gmail.com>
2024-02-05 19:43:32 +0000

    COmpleted project

--- public/browser-app.js
+++ public/browser-app.js
@@ -6,7 +6,7 @@ const total_amount = 10998;
 const shipping_fee = 1099;
 
 var stripe = Stripe(
-  'pk_test_51I87djFp5pnuKUXgBVIHiR36vVAWyfuyb7ckrhgyDNA1kM0GWHas9ZGUAgwJSFNUxrbyE6NwlMNmls1iGSfzHDdE00DQB3y6AH'
+  'pk_test_51OgWg6KlEhWxT9MqhwF7DibgDeGBBCMvJoElFIPSw66DXtKFjEfqWQkiObVXd1YuuuvaKTH0GzhmYss4ZMEjZCql00HbSOvuIo'
 );
 
 // The items the customer wants to buy
